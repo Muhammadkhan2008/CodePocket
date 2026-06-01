8c8d551a07394f3cf7d3e0d46b57680bad429318
Muhammad Khan <khanmuhammadkolachi390@gmail.com>
2026-06-01 15:42:25 +0500

    Add Capacitor config

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.muhammadkhan.codepocket',
+  appName: 'CodePocket',
+  webDir: 'dist'
+};
+
+export default config;
